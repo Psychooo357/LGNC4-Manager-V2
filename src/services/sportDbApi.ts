9932affc55cd7a5e680139958767cbd188e66b35
Psychooo357 <kilyan357@gmail.com>
2026-08-03 19:20:27 +0200

    Use Cloudflare Worker proxy

--- src/services/sportDbApi.ts
+++ src/services/sportDbApi.ts
@@ -1,15 +1,7 @@
-const API_KEY = import.meta.env.VITE_SPORTDB_API_KEY;
-
-const BASE_URL = "https://api.sportdb.dev";
-
-const headers = {
-    "X-API-Key": API_KEY,
-};
+const BASE_URL = "https://lgnc4-api.kilyan357.workers.dev";
 
 async function fetchApi(endpoint: string) {
-    const response = await fetch(`${BASE_URL}${endpoint}`, {
-        headers,
-    });
+    const response = await fetch(`${BASE_URL}${endpoint}`);
 
     if (!response.ok) {
         throw new Error(`Erreur SportDB : ${response.status}`);
@@ -19,19 +11,13 @@ async function fetchApi(endpoint: string) {
 }
 
 export function getLigue3Standings() {
-    return fetchApi(
-        "/api/flashscore/football/france:77/ligue-3:pv9Nf2KF/2026-2027/standings"
-    );
+    return fetchApi("/standings");
 }
 
 export function getLigue3Results() {
-    return fetchApi(
-        "/api/flashscore/football/france:77/ligue-3:pv9Nf2KF/2026-2027/results?page=1"
-    );
+    return fetchApi("/results?page=1");
 }
 
 export function getLigue3Fixtures() {
-    return fetchApi(
-        "/api/flashscore/football/france:77/ligue-3:pv9Nf2KF/2026-2027/fixtures?page=1"
-    );
+    return fetchApi("/fixtures?page=1");
 }
